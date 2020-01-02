8f6e3f1778b6d741a18f4b3478379e29b59e249b
Cian O'Gralaigh <ihatecian@gmail.com>
2020-01-02 20:23:55 +0000

    add html response

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('Hello World');
-  process.exit();
+  console.log(req.url, req.method, req.headers);
+  //process.exit();
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
