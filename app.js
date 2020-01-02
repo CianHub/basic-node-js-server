14fd4b8346cfcef12307bd61ec0f494af78ac3dd
Cian O'Gralaigh <ihatecian@gmail.com>
2020-01-02 20:30:36 +0000

    Add routes

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
   //process.exit();
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('</html>');
-  res.end();
+  const url = req.url;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Index</title></head>');
+    res.write('<body><h1>Index</h1></body>');
+    res.write('</html>');
+    res.end();
+  } else {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Not The Index</title></head>');
+    res.write('<body><h1>Not The Index</h1></body>');
+    res.write('</html>');
+    res.end();
+  }
 });
 
 server.listen(3000);
