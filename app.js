19968da63e7380354f86bf8d76121c857b314679
Cian O'Gralaigh <ihatecian@gmail.com>
2020-01-02 20:39:20 +0000

    Add specific route behaviour and file writing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   //process.exit();
   const url = req.url;
+  const method = req.method;
   if (url === '/') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
@@ -11,14 +13,19 @@ const server = http.createServer((req, res) => {
       '<body><h1>Index</h1><form action="/1" method="POST"><input type="text" name="input"><button type="submit">Click</button></form></body>'
     );
     res.write('</html>');
-    res.end();
+    return res.end();
+  } else if (url === '/1' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
   } else {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Not The Index</title></head>');
     res.write('<body><h1>Not The Index</h1></body>');
     res.write('</html>');
-    res.end();
+    return res.end();
   }
 });
 
